e42ba7b627c4ae83e6646283164875c2b6c90874
kivi98 <kiviamarakoon@gmail.com>
2025-08-03 12:19:58 +0530

    Small UI changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             justifyContent: "center",
             alignItems: "center",
           }}
-          spacing={4}
+          spacing={0}
         >
           <Grid item xs={12} md={4}>
             <Box
